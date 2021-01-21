16f78c31b216a16c3383c6c9d7d91b8330eda7b0
Kohei Tanaka <kohei69@gmail.com>
2021-01-21 21:21:21 +0900

    init

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+import Vue from 'vue';
+import MyComponent from '@/my-component.vue';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    MyComponent
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <my-component />
+  </div>
+</template>
